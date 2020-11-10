b65e9c85718658498fb6f088db3a125057766751
tomomi ogino <s4tm83@gmail.com>
2020-11-10 14:55:12 +0900

    add:  tweet詳細画面の作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show]
   def index
     @tweets = Tweet.all.order(created_at: :desc)
   end
@@ -9,8 +10,13 @@ class TweetsController < ApplicationController
     Tweet.create(tweet_params)
     redirect_to new_tweet_path
   end
+  def show
+  end
   private
   def tweet_params
     params.require(:tweet).permit(:content)
+  end
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end

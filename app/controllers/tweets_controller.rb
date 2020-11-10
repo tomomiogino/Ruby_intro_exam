23ffe717180129e16800da886b21e0a9a8fe08cf
tomomi ogino <s4tm83@gmail.com>
2020-11-10 12:59:45 +0900

    add: tweet作成機能を実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,12 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+  def create
+    Tweet.create(tweet_params)
+    redirect_to new_tweet_path
+  end
+  private
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
 end
